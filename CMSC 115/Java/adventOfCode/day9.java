d0cf7618b3649142b4d4fdcf9a0c24391d878575
Tarquinen <dannysmo@gmail.com>
2023-12-09 21:24:25 +0100

    advent day 9

--- CMSC 115/Java/adventOfCode/day9.java
+++ CMSC 115/Java/adventOfCode/day9.java
@@ -5,8 +5,7 @@ package adventOfCode;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+
 
 public class day9 {
     public static void main (String[] args) throws Exception {

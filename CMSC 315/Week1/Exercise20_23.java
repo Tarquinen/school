026cb925cd47bd36a669bb79349f192144cf4ac9
Tarquinen <dannysmo@gmail.com>
2024-03-14 23:29:17 +0100

    bugfix

--- CMSC 315/Week1/Exercise20_23.java
+++ CMSC 315/Week1/Exercise20_23.java
@@ -43,62 +43,62 @@ public class Exercise20_23 {
 
         // Phase 1: Scan tokens
         for (String token: tokens) {
-        if (token.length() == 0) // Blank space
-            continue; // Back to the for loop to extract the next token
-        else if (token.charAt(0) == '+' || token.charAt(0) == '-') {
-            // Process all +, -, *, /, ^, % in the top of the operator stack 
-            while (!operatorStack.isEmpty() &&
-            (operatorStack.peek() == '+' || 
-            operatorStack.peek() == '-' ||
-            operatorStack.peek() == '*' ||
-            operatorStack.peek() == '/' ||
-            operatorStack.peek() == '^' ||
-            operandStack.peek() == '%')) {
-            processAnOperator(operandStack, operatorStack);
-            }
-
-            // Push the + or - operator into the operator stack
-            operatorStack.push(token.charAt(0));
-        }
-        else if (token.charAt(0) == '*' || token.charAt(0) == '/' || token.charAt(0) == '%') {
-            // Process all *, /, % in the top of the operator stack 
-            while (!operatorStack.isEmpty() &&
-            (operatorStack.peek() == '*' ||
-            operatorStack.peek() == '/' ||
-            operatorStack.peek() == '%')) {
-            processAnOperator(operandStack, operatorStack);
-            }
-
-            // Push the *, / or % operator into the operator stack
-            operatorStack.push(token.charAt(0));
-        }
-            
-        else if (token.charAt(0) == '^') {
-            // process all ^ in the top of the operator stack
-            while (!operatorStack.isEmpty() &&
-            operatorStack.peek() == '^') {
+            if (token.length() == 0) // Blank space
+                continue; // Back to the for loop to extract the next token
+            else if (token.charAt(0) == '+' || token.charAt(0) == '-') {
+                // Process all +, -, *, /, ^, % in the top of the operator stack 
+                while (!operatorStack.isEmpty() &&
+                (operatorStack.peek() == '+' || 
+                operatorStack.peek() == '-' ||
+                operatorStack.peek() == '*' ||
+                operatorStack.peek() == '/' ||
+                operatorStack.peek() == '^' ||
+                operandStack.peek() == '%')) {
                 processAnOperator(operandStack, operatorStack);
-            }
-            
-            // push the ^ operator in the operator stack
-            operatorStack.push(token.charAt(0));
-        }
+                }
 
-        else if (token.trim().charAt(0) == '(') {
-            operatorStack.push('('); // Push '(' to stack
-        }
-        else if (token.trim().charAt(0) == ')') {
-            // Process all the operators in the stack until seeing '('
-            while (operatorStack.peek() != '(') {
-            processAnOperator(operandStack, operatorStack);
+                // Push the + or - operator into the operator stack
+                operatorStack.push(token.charAt(0));
             }
-            
-            operatorStack.pop(); // Pop the '(' symbol from the stack
-        }
-        else { // An operand scanned
-            // Push an operand to the stack
-            operandStack.push(Integer.valueOf(token));
-        }
+            else if (token.charAt(0) == '*' || token.charAt(0) == '/' || token.charAt(0) == '%') {
+                // Process all *, /, % in the top of the operator stack 
+                while (!operatorStack.isEmpty() &&
+                (operatorStack.peek() == '*' ||
+                operatorStack.peek() == '/' ||
+                operatorStack.peek() == '%')) {
+                processAnOperator(operandStack, operatorStack);
+                }
+
+                // Push the *, / or % operator into the operator stack
+                operatorStack.push(token.charAt(0));
+            }
+                
+            else if (token.charAt(0) == '^') {
+                // process all ^ in the top of the operator stack
+                while (!operatorStack.isEmpty() &&
+                operatorStack.peek() == '^') {
+                    processAnOperator(operandStack, operatorStack);
+                }
+                
+                // push the ^ operator in the operator stack
+                operatorStack.push(token.charAt(0));
+            }
+
+            else if (token.trim().charAt(0) == '(') {
+                operatorStack.push('('); // Push '(' to stack
+            }
+            else if (token.trim().charAt(0) == ')') {
+                // Process all the operators in the stack until seeing '('
+                while (operatorStack.peek() != '(') {
+                processAnOperator(operandStack, operatorStack);
+                }
+                
+                operatorStack.pop(); // Pop the '(' symbol from the stack
+            }
+            else { // An operand scanned
+                // Push an operand to the stack
+                operandStack.push(Integer.valueOf(token));
+            }
         }
 
         // Phase 2: process all the remaining operators in the stack 
@@ -118,17 +118,17 @@ public class Exercise20_23 {
         int op1 = operandStack.pop();
         int op2 = operandStack.pop();
         if (op == '+') 
-        operandStack.push(op2 + op1);
+            operandStack.push(op2 + op1);
         else if (op == '-') 
-        operandStack.push(op2 - op1);
+            operandStack.push(op2 - op1);
         else if (op == '*') 
-        operandStack.push(op2 * op1);
+            operandStack.push(op2 * op1);
         else if (op == '/') 
-        operandStack.push(op2 / op1);
+            operandStack.push(op2 / op1);
         else if (op == '%') 
-        operandStack.push(op2 % op1);
+            operandStack.push(op2 % op1);
         else if (op == '^') 
-        operandStack.push((int)Math.pow(op2, op1));
+            operandStack.push((int)Math.pow(op2, op1));
     }
     
     public static String insertBlanks(String s) {

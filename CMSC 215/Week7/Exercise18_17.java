e7e6bcd7d6bef28c3a55deaa88cadf4a6fed3ee8
tarqin <russiandanrs@gmail.com>
2024-03-21 21:12:16 +0100

    .

--- CMSC 215/Week7/Exercise18_17.java
+++ CMSC 215/Week7/Exercise18_17.java
@@ -1,19 +1,16 @@
 import java.util.Scanner;
 
-
-
-
 public class Exercise18_17 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter a String: ");
-        String in = input.nextLine();
-        System.out.println("Enter a character: ");
+        System.out.print("Enter a String: ");
+        String inString = input.nextLine();
+        System.out.print("Enter a character: ");
         String inChar = input.nextLine();
-        char[] chars = in.toCharArray();
+        char[] chars = inString.toCharArray();
         char ch = inChar.charAt(0);
         input.close();
-        System.out.println(count(chars, ch));
+        System.out.println(ch + " appears " + count(chars, ch) + " times");
     }
 
     public static int count(char[] chars, char ch) {

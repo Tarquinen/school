9cba3722864db8b62280afed3db2f1bf9373204c
tarqin <russiandanrs@gmail.com>
2024-03-27 05:22:11 +0100

    gift wrapping

--- CMSC 315/Week3/GiftWrapping.java
+++ CMSC 315/Week3/GiftWrapping.java
@@ -1,54 +1,112 @@
-// package CMSC315.Week3;
-
 import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
-import javafx.scene.Scene;
 import javafx.scene.input.MouseButton;
-
+import java.util.ArrayList;
 
 public class GiftWrapping extends Application {
-    static int[][] dotCoords;
+    static ArrayList<Integer[]> dotCoords = new ArrayList<Integer[]>();
+    static ArrayList<Integer[]> H = new ArrayList<Integer[]>();
     public void start(Stage primaryStage) {
-        int dotCount = 20;
-        dotCoords = new int[20][2]; // [index][0:x, 1:y]
+        int initialDotCount = 5;
         Pane pane = new Pane();
-        for (int i = 0; i < dotCount; i++) {
+        for (int i = 0; i < initialDotCount; i++) {
             Circle dot = new Circle(5, Color.RED);
             int x = (int)(Math.random() * 400);
             int y = (int)(Math.random() * 400);
-            dotCoords[i][0] = x;
-            dotCoords[i][1] = y;
+            dotCoords.add(new Integer[]{x, y});
 
             dot.setCenterX(x);
             dot.setCenterY(y);
             pane.getChildren().add(dot);
 
             dot.setOnMouseClicked(e -> {
-                if (e.getButton() == MouseButton.PRIMARY) {
-                    System.out.println("Dot clicked at " + x + " " + y);
-                }
-                else {
-                    System.out.println("Dot right clicked at " + x + " " + y);
+                if (e.getButton() == MouseButton.SECONDARY) {
                     pane.getChildren().remove(dot);
-                    dotCoords[i][0] = -1;
-                    // dotCoords[i][1] = -1;
+                    removeDot(x, y);
                 }
             });
         }
+        System.out.println(rightMostLowest()[0] + ", " + rightMostLowest()[1]);
+        H.add(rightMostLowest());
+        System.out.println(H.get(0));
+    
+        pane.setOnMouseClicked(e -> {
+            if (e.getButton() == MouseButton.PRIMARY && !duplicateDot((int)e.getX(), (int)e.getY())){
+                Circle dot = new Circle(5, Color.RED);
+                int x = (int)e.getX();
+                int y = (int)e.getY();
+
+                System.out.println("Adding dot at " + x + " " + y);
+                System.out.println("Array after addition");
+                dotCoords.add(new Integer[]{x, y});
+
+                //print array
+                for (int j = 0; j < dotCoords.size(); j++) {
+                    System.out.println(dotCoords.get(j)[0] + " " + dotCoords.get(j)[1]);
+                }
+
+                dot.setCenterX(x);
+                dot.setCenterY(y);
+                pane.getChildren().add(dot);
+
+                dot.setOnMouseClicked(f -> {
+                    if (f.getButton() == MouseButton.SECONDARY) {
+                        pane.getChildren().remove(dot);
+                        removeDot(x, y);
+                    }
+                });
+            }
+        });
 
         Scene scene = new Scene(pane, 400, 400);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
-    public static void main(String[] args) {
-        launch(args);
-        for (int i = 0; i < dotCoords.length; i++) {
-            System.out.println(dotCoords[i][0] + " " + dotCoords[i][1]);
+    public static boolean duplicateDot(int x, int y) {
+        for (int i = 0; i < dotCoords.size(); i++) {
+            if (dotCoords.get(i)[0] == x && dotCoords.get(i)[1] == y) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void removeDot(int x, int y) {
+        for (int i = 0; i < dotCoords.size(); i++) {
+            if (dotCoords.get(i)[0] == x && dotCoords.get(i)[1] == y) {
+                dotCoords.remove(i);
+                System.out.println("Removing dot at " + x + " " + y);
+                //print array
+                System.out.println("Array after removal");
+                for (int j = 0; j < dotCoords.size(); j++) {
+                    System.out.println(dotCoords.get(j)[0] + " " + dotCoords.get(j)[1]);
+                }
+            }
         }
     }
-    
+    public static void sortPoints() {
+
+    }
+
+    public static Integer[] rightMostLowest () {
+        Integer[] lowest = dotCoords.get(0);
+        for (Integer[] p : dotCoords) {
+            if (p[1] > lowest[1]) {
+                lowest = p;
+            }
+            else if (p[1] == lowest[1]) {
+                if (p[0] > lowest[0]) lowest = p;
+            }
+        }
+        return lowest;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
